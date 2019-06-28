1ed83efcf937036352bfb60b6894a6aad285435f
Jinhwa Roh <nnjh12@gmail.com>
2019-06-28 14:16:44 -0400

    initial file

---
+++ models/googleBook.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const googleBookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: String,
+  image: String,
+  link: String,
+  favorite: { type: Boolean, default: false},
+  date: { type: Date, default: Date.now }
+});
+
+const googleBook = mongoose.model("Book", googleBookSchema);
+
+module.exports = googleBook;

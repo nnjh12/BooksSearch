3127a0348d78de5916c96db28276b24cd0a4e2ed
Jinhwa Roh <nnjh12@gmail.com>
2019-06-29 00:44:00 -0400

    working API (googleBookAPI)

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,13 +2,10 @@ import React from "react";
 
 function Jumbotron({ children }) {
   return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
-      {children}
+    <div className="jumbotron">
+      { children }
     </div>
-  );
+      );
 }
 
 export default Jumbotron;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
 export default {
+  search: function(query) {
+    return axios.get(BASEURL + query);
+  },
   // Gets all books
   getBooks: function() {
     return axios.get("/api/books");
